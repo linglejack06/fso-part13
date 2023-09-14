fd8367c638bdcd3b4802d30d1c36ca9b2343cceb
Jack Lingle <captainjack0818@gmail.com>
2023-09-14 18:40:24 -0400

    add model and get, delete, and post requests

--- blog-sql/index.js
+++ blog-sql/index.js
@@ -1,4 +1,4 @@
-const { Sequelize, DataTypes, Model, QueryTypes } = require("sequelize");
+const { Sequelize, DataTypes, Model } = require("sequelize");
 const express = require("express");
 
 const app = express();
@@ -7,14 +7,62 @@ app.use(express.json());
 const sequelize = new Sequelize(
   "postgres://jack:sqlisbest@localhost:5432/postgres"
 );
+class Blog extends Model {}
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    author: {
+      type: DataTypes.TEXT,
+    },
+    url: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    likes: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: "blog",
+  }
+);
+Blog.sync();
 
 app.get("/api/blogs", async (req, res) => {
-  await sequelize.authenticate();
-  const blogs = await sequelize.query("SELECT * FROM blogs", {
-    type: QueryTypes.SELECT,
-  });
-  res.send(blogs);
-  sequelize.close();
+  try {
+    const blogs = await Blog.findAll();
+    res.json(blogs);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+});
+app.post("/api/blogs", async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body);
+    res.json(blog);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+});
+app.delete("/api/blogs/:id", async (req, res) => {
+  try {
+    await Blog.destroy({ where: { id: req.params.id } });
+    res.status(200).end();
+  } catch (error) {
+    res.status(400).json({ error });
+  }
 });
 
 app.listen(3000, () => {
