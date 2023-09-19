613c7632e7bd0f5dd385b3fb5c79ea854ce4b68d
Jack Lingle <captainjack0818@gmail.com>
2023-09-19 19:02:44 -0400

    add foreign keys for join queries

--- notes-sql/models/index.js
+++ notes-sql/models/index.js
@@ -1,8 +1,11 @@
 const Note = require("./note");
 const User = require("./user");
-
-Note.sync();
-User.sync();
+// assigns foreign keys to each model for join queries
+User.hasMany(Note);
+Note.belongsTo(User);
+// causes data in database to match model
+Note.sync({ alter: true });
+User.sync({ alter: true });
 
 module.exports = {
   Note,
