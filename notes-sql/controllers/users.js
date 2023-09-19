f3bcac079feb33d5160ac90ab1016b92129df468
Jack Lingle <captainjack0818@gmail.com>
2023-09-19 18:52:37 -0400

    add user model and router

---
+++ notes-sql/controllers/users.js
@@ -0,0 +1,31 @@
+const usersRouter = require("express").Router();
+const { User } = require("../models");
+
+usersRouter.get("/", async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.json(users);
+  } catch (error) {
+    res.status(400).send({ error });
+  }
+});
+
+usersRouter.post("/", async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.json(user);
+  } catch (error) {
+    res.status(400).send({ error });
+  }
+});
+
+usersRouter.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+    res.json(user);
+  } catch (error) {
+    res.status(400).send({ error });
+  }
+});
+
+module.exports = usersRouter;
--- notes-sql/index.js
+++ notes-sql/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const { PORT } = require("./util/config");
 const { connectToDatabase } = require("./util/db");
 const notesRouter = require("./controllers/notes");
+const usersRouter = require("./controllers/users");
 
 const app = express();
 app.use(express.json());
 
 app.use("/api/notes", notesRouter);
+app.use("/api/users", usersRouter);
 
 const start = async () => {
   await connectToDatabase();
