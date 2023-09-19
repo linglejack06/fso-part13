981af29af2c90c77499e2cee542a21b088e2a9aa
Jack Lingle <captainjack0818@gmail.com>
2023-09-19 19:00:06 -0400

    add login router to handle tokens

---
+++ notes-sql/controllers/login.js
@@ -0,0 +1,33 @@
+const loginRouter = require("express").Router();
+// eslint-disable-next-line import/no-unresolved
+const jwt = require("jsonwebtoken");
+const { SECRET } = require("../util/config");
+const User = require("../models");
+
+loginRouter.post("/", async (req, res) => {
+  const { body } = req;
+  try {
+    const user = await User.findOne({
+      where: {
+        username: body.username,
+      },
+    });
+    const pwCorrect = body.password === "secret";
+    if (!(user && pwCorrect)) {
+      res.status(401).json({
+        error: "Invalid username or password",
+      });
+    }
+    const userForToken = {
+      username: user.username,
+      id: user.id,
+    };
+
+    const token = jwt.sign(userForToken, SECRET);
+    res.status(200).send({ token, username: user.username, name: user.name });
+  } catch (error) {
+    res.status(400).send({ error });
+  }
+});
+
+module.exports = loginRouter;
