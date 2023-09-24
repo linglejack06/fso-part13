179be9d218c1843c9f50307f77c12984ac8dc856
Jack Lingle <captainjack0818@gmail.com>
2023-09-24 11:31:00 -0400

    add many to many relationship to teams/users through membership connection table

--- notes-sql/models/index.js
+++ notes-sql/models/index.js
@@ -1,13 +1,17 @@
 const Note = require("./note");
 const User = require("./user");
+const Team = require("./team");
+const Membership = require("./membership");
 // assigns foreign keys to each model for join queries
 User.hasMany(Note);
 Note.belongsTo(User);
 
-Note.sync();
-User.sync();
+User.belongsToMany(Team, { through: Membership });
+Team.belongsToMany(User, { through: Membership });
 
 module.exports = {
   Note,
   User,
+  Team,
+  Membership,
 };
